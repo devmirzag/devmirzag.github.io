15cd135311099db499553b98314c89a2bac03b0f
devmirzag <180986@students.au.edu.pk>
2023-09-20 23:20:38 +0500

    Updates

--- src/pages/Blog/blog.js
+++
@@ -1,9 +0,0 @@
-function Blog() {
-  return (
-    <div className="blog">
-      <p>protected by copyright © 2001-2023 Namecheap.com</p>
-    </div>
-  )
-}
-
-export default Blog
--- src/pages/Contact/contact.js
+++
@@ -1,9 +0,0 @@
-function Contact() {
-  return (
-    <div className="contact">
-      <p>protected by copyright © 2001-2023 Namecheap.com</p>
-    </div>
-  )
-}
-
-export default Contact
